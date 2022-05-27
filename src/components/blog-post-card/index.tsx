aec09151baf01b15b64bb9a2a7f2d10ef26c766b
mlaajaj <99878858+mlaajaj@users.noreply.github.com>
2022-05-27 22:55:54 +0200

    Update index.tsx

--- src/components/blog-post-card/index.tsx
+++ src/components/blog-post-card/index.tsx
@@ -1,12 +1,13 @@
-import Link from 'next/link';
 import {
-  LinkBox,
-  LinkOverlay,
-  Heading,
-  Text,
-  VStack,
-  HStack,
-} from '@chakra-ui/react';
+  Flex,
+    Box,
+    Heading,
+    Text,
+    useColorModeValue,
+    VStack,
+    HStack,
+    Link,
+  } from '@chakra-ui/react';
 import { format } from 'timeago.js';
 
 import { BlogPost } from '@/types/blog-post';
@@ -17,54 +18,79 @@ const BlogPostCard = ({
   slug,
   date,
   readingTime,
+  img,
 }: BlogPost) => {
   return (
-    <LinkBox as="article">
-      <VStack
-        alignItems="stretch"
-        w="full"
-        p={{ base: 0, md: 4 }}
-        _hover={{
-          bg: 'gray.100',
-          transform: 'scale(1.025, 1.025)',
-        }}
-        _dark={{
-          _hover: {
-            bg: 'gray.700',
-          },
-        }}
-        rounded="md"
-        transitionDuration="slow"
-        transitionProperty="all"
-        transitionTimingFunction="ease-out"
-      >
-        <VStack alignItems="flex-start">
-          <Link href={`/blog/${slug}`} passHref>
-            <LinkOverlay>
-              <Heading size="md">{title}</Heading>
-            </LinkOverlay>
-          </Link>
-          <HStack
-            divider={
-              <Text mx={2} color="gray.500">
-                •
-              </Text>
-            }
-          >
-            <Text color="gray.500" fontSize="sm">
-              {format(date)}
-            </Text>
-            <Text color="gray.500" fontSize="sm">
-              {readingTime}
-            </Text>
-          </HStack>
-        </VStack>
-        <Text color="gray.500" fontSize="sm">
-          {description}
-        </Text>
-      </VStack>
-    </LinkBox>
+    <Flex
+    p={10}
+    w="full"
+    alignItems="center"
+    justifyContent="center"
+  >
+    <Box
+      mx="auto"
+      w='100%'
+      p={4}
+      rounded="lg"
+      shadow="xl"
+      bg={'#FFFF'}      
+      maxW="2xl"
+    >
+
+      <Flex justifyContent="space-between" alignItems="center">
+        <span
+          color={useColorModeValue("gray.600", "gray.400")}
+        >
+          {format(date)}
+        </span>
+        <Link
+          href={`/blog/${slug}`}
+          px={3}
+          py={1}
+          bg="gray.600"
+          color="gray.100"
+          fontSize="sm"
+          fontWeight="700"
+          rounded="md"
+          _hover={{ bg: "gray.500" }}
+        >
+          {readingTime}
+        </Link>
+      </Flex>
+
+      <Box mt={2}>
+        <Link
+          href={`/blog/${slug}`}
+          fontSize="2xl"
+          color={useColorModeValue("gray.700", "white")}
+          fontWeight="700"
+          _hover={{
+            color: useColorModeValue("gray.600", "gray.200"),
+            textDecor: "underline",
+          }}
+        >
+          {title}
+        </Link>
+        <p color={useColorModeValue("gray.600", "gray.300")}>
+              {description}
+        </p>
+      </Box>
+
+      <Flex justifyContent="space-between" alignItems="center" mt={4}>
+        <Link
+          color={useColorModeValue("brand.600", "brand.400")}
+          _hover={{ textDecor: "underline" }}
+        >
+          Lire l'article
+        </Link>
+
+      </Flex>
+    </Box>
+  </Flex>
   );
 };
 
 export default BlogPostCard;
+
+
+
